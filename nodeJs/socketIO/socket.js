eb804cd6ab983431e1701a7d1651cdf9eaaf66cc
sangja21 <ta7538@naver.com>
2025-03-04 08:52:57 +0900

    [FEAT] : socket.io 통신

---
+++ nodeJs/socketIO/socket.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  connectionStateRecovery: {},
+});
+
+// ES Modules에서 __dirname 설정
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(3007, () => {
+  console.log("server running at http://localhost:3007");
+});
